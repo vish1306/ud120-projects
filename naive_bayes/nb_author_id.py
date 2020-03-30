141d1fe67585745f0f085843c6d5fd96f17648e3
vish1306 <62842140+vish1306@users.noreply.github.com>
2020-03-30 23:50:58 +0530

    Complete Naive_Bayes mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -19,7 +19,13 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+features_train, features_test, labels_train, labels_test = preprocess(test_size=0.3)
+from email_preprocess import accuracy_score
+clf=preprocess()
+clf.fit(features_train,label_train)
+pred=clf.predict(features_test)
+print accuracy_score(pred,label_test)
+
 
 
 
